170ff33ad46ca54c92d29919aa0137dea090b5cd
Ellen <ellen.brage@welcom.se>
2025-06-09 21:00:07 +0200

    Install and use Montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import { BookContextProvider } from "./context/BookContext";
+
+const montserrat = Montserrat({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
 	title: "The Classics Library",
@@ -16,10 +22,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className="antialiased bg-orange-100">
+			<body className={`${montserrat.variable} antialiased bg-orange-100`}>
 				<Navigation />
 				<BookContextProvider>
-					<main className="flex flex-col items-center justify-center pb-12 md:pb-24 min-h-[82vh] mx-3 md:mx-5 text-center">
+					<main className="font-montserrat flex flex-col items-center justify-center pb-12 md:pb-24 min-h-[82vh] mx-3 md:mx-5 text-center">
 						<div className="w-full max-w-200 flex flex-col items-center">
 							{children}
 						</div>
